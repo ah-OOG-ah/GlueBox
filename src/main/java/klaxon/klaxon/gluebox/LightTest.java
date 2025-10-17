053fedde996e86cc488d56fb9d9f85bf7abc0648
ah-OOG-ah <75745146+ah-OOG-ah@users.noreply.github.com>
2025-10-17 19:20:38 -0400

    Fix tests (thanks Overmind)

--- src/main/java/klaxon/klaxon/gluebox/LightTest.java
+++ src/main/java/klaxon/klaxon/gluebox/LightTest.java
@@ -1,11 +1,15 @@
 package klaxon.klaxon.gluebox;
 
+import static org.lwjgl.opengl.GL11.GL_ENABLE_BIT;
 import static org.lwjgl.opengl.GL11.GL_LIGHT0;
+import static org.lwjgl.opengl.GL11.GL_LIGHTING_BIT;
 import static org.lwjgl.opengl.GL11.glGetBoolean;
+import static org.lwjgl.opengl.GL11.glIsEnabled;
+import static org.lwjgl.opengl.GL11.glIsEnabled;
 import static org.lwjgl.opengl.GL11.glPopAttrib;
 import static org.lwjgl.opengl.GL11.glPushAttrib;
-import static org.lwjgl.opengl.GL11C.glDisable;
-import static org.lwjgl.opengl.GL11C.glEnable;
+import static org.lwjgl.opengl.GL11.glDisable;
+import static org.lwjgl.opengl.GL11.glEnable;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -24,10 +28,11 @@ public class LightTest extends GlueBoxExtension {
         final int maxLights = GL11.glGetInteger(GL11.GL_MAX_LIGHTS);
         for (int i = 0; i < maxLights; i++) {
             glDisable(GL_LIGHT0 + i);
+            assertLog(!glIsEnabled(GL_LIGHT0 + i), "Init failed!");
         }
 
         // Push the bits and make sure they haven't changed
-        glPushAttrib(GL11.GL_ENABLE_BIT);
+        glPushAttrib(GL_ENABLE_BIT);
         for (int i = 0; i < maxLights; ++i) {
             assertLog(!glGetBoolean(GL_LIGHT0 + i), "Passthrough failed!");
         }
@@ -40,9 +45,7 @@ public class LightTest extends GlueBoxExtension {
 
         // And pop them, making sure they get unset
         glPopAttrib();
-
         for (int i = 0; i < maxLights; ++i) {
-            glEnable(GL_LIGHT0 + i);
             assertLog(!glGetBoolean(GL_LIGHT0 + i), "Failed to pop bit state!");
         }
     }
